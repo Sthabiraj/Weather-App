7c739b73d1270e7121b802243a777b20b837aea3
Sthabiraj <birajshrestha2007@gmail.com>
2023-07-12 21:53:47 +0545

    City weather fetched

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
 async function fetchData(cityName) {
+  // Fetching weather data on the basis of city name
   const apiKey = "b9042ec5d9a26c6e11c152ed3cf8ec90";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
+
+  // For current day and date
+  const date = new Date();
+  let weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  // Define the options for formatting the date
+  let options = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+
+  return {
+    city: data.name, // City name
+    day: weekdays[date.getDay()], // Current day
+    date: date.toLocaleDateString("en-US", options), // Current date
+    condition: data.weather[0].description, // Weather condition
+    icon: data.weather[0].icon, // Weather icon
+    temp: data.main.temp, // City temperature
+    pressure: data.main.pressure, // Pressure
+    windSpeed: data.wind.speed, // Wind speed
+    humidity: data.main.humidity, // Humidity
+  };
 }
-fetchData("Chelmsford");
+
+fetchData("Chelmsford")
+  .then((weather) => {
+    console.log(weather);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
