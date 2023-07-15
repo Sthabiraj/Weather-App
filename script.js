910176d29a6664dc940437a1e52d27d969d16513
Sthabiraj <birajshrestha2007@gmail.com>
2023-07-15 22:44:16 +0545

    icon added

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-async function fetchData(cityName) {
-  // Fetching weather data on the basis of city name
+async function fetchData(cityName = "Chelmsford") {
+  // Fetching weather data based on the city name
   const apiKey = "b9042ec5d9a26c6e11c152ed3cf8ec90";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
   const response = await fetch(url);
   const data = await response.json();
 
-  console.log(data.name);
+  console.log(data);
 
-  // For current day and date
-  const date = new Date();
+  // For the current day and date
+  const currentDate = new Date();
   let weekdays = [
     "Sunday",
     "Monday",
@@ -25,10 +25,10 @@ async function fetchData(cityName) {
     day: "numeric",
   };
 
-  return {
+  const weather = {
     name: data.name, // City name
-    day: weekdays[date.getDay()], // Current day
-    date: date.toLocaleDateString("en-US", options), // Current date
+    day: weekdays[currentDate.getDay()], // Current day
+    date: currentDate.toLocaleDateString("en-US", options), // Current date
     condition: data.weather[0].description, // Weather condition
     icon: data.weather[0].icon, // Weather icon
     temp: data.main.temp, // City temperature
@@ -36,11 +36,21 @@ async function fetchData(cityName) {
     windSpeed: data.wind.speed, // Wind speed
     humidity: data.main.humidity, // Humidity
   };
+
+  document.querySelector("#condition").innerHTML = weather.condition;
+  document.querySelector("#temperature").innerHTML = Math.round(weather.temp);
+  document.querySelector("#date").innerHTML = weather.date;
+  document.querySelector("#day").innerHTML = weather.day;
+  document.querySelector("#city-name").innerHTML = weather.name;
+  document.querySelector("#pressure").innerHTML = weather.pressure;
+  document.querySelector("#wind-speed").innerHTML = weather.windSpeed;
+  document.querySelector("#humidity").innerHTML = weather.humidity;
 }
-fetchData("chelmsford")
-  .then((city) => {
-    console.log(city);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+
+fetchData("Chelmsford");
+
+const city = document.querySelector("#search-box");
+function searchWeather() {
+  fetchData(city.value);
+}
+city.innerText = "";
