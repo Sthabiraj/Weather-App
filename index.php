b166a3779b432e3ea19f57b660940bf2f72bce4a
Sthabiraj <birajshrestha2007@gmail.com>
2023-08-11 23:21:29 +0545

    function to insert value to mysql added

--- index.php
+++ index.php
@@ -1,71 +1,142 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Weather Forecast App</title>
-    <link rel="stylesheet" href="style.css" />
-    <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-    />
-  </head>
-  <body>
-    <main class="container">
-      <!-- Search section -->
-      <section class="search-section">
-        <input type="search" id="search-box" placeholder="eg. London" />
-        <button id="button" onclick="searchWeather()">Search</button>
-      </section>
-      <!-- End of Search section -->
+<?php
+$city_name = "Chelmsford";
+function fetch_weather_data()
+{
+  global $city_name; // Make the $city_name variable accessible within the function
+  $api_key = "b9042ec5d9a26c6e11c152ed3cf8ec90";
+  $url = "https://api.openweathermap.org/data/2.5/weather?q=$city_name&appid=$api_key&units=metric";
 
-      <!-- City Weather -->
-      <section class="city-weather group">
-        <h2><span id="condition"></span></h2>
-        <figure>
-          <img sr="" alt="weather icon" id="weather-icon" />
-        </figure>
-        <h1><span id="temperature"></span>°C</h1>
-        <p><span id="day"></span>, <span id="date"></span></p>
-        <h2>
-          <span class="material-symbols-outlined"> location_on </span>
-          <span id="city-name"></span>
-        </h2>
-      </section>
-      <!-- End of City Weather -->
-      <hr class="group" />
-      <!-- Weather info -->
-      <section class="weather-info group">
-        <!-- Pressure -->
-        <div class="info-item">
-          <figure><img src="./icons/pressure.png" alt="info icon" /></figure>
-          <h1><span id="pressure"></span> Pa</h1>
-          <p>Pressure</p>
-        </div>
-        <!-- Wind speed -->
-        <div class="info-item">
-          <figure><img src="./icons/wind speed.png" alt="info icon" /></figure>
-          <h1><span id="wind-speed"></span> m/s</h1>
-          <p>Wind</p>
-        </div>
-        <!-- Humidity -->
-        <div class="info-item">
-          <figure><img src="./icons/humidity.png" alt="info icon" /></figure>
-          <h1><span id="humidity"></span> %</h1>
-          <p>Humidity</p>
-        </div>
-      </section>
-      <!-- End of weather info -->
+  // Reads the JSON file
+  $json_data = file_get_contents($url);
 
-      <!-- Error section -->
-      <section class="error hide">
-        <figure>
-          <img src="./icons/error.png" alt="Error icon" />
-        </figure>
-        <p>Invalid city. Please enter a valid city name.</p>
-      </section>
-      <!-- End of Error Section -->
-    </main>
-    <script src="script.js"></script>
-  </body>
-</html>
+  // Decodes the JSON data to PHP object
+  $response_data = json_decode($json_data);
+
+  // Check if the API request was successful
+  if ($response_data->cod === 200) {
+    $day_of_week = date('l');
+    $day_and_date = date('M j, Y');
+    $weather_condition = $response_data->weather[0]->description;
+    $weather_icon = $response_data->weather[0]->icon;
+    $temperature = $response_data->main->temp;
+    $pressure = $response_data->main->pressure;
+    $wind_speed = $response_data->wind->speed;
+    $humidity = $response_data->main->humidity;
+
+    // Construct the complete URL for the icon
+    $icon_url = "https://openweathermap.org/img/w/$weather_icon.png";
+    // Return data as tuple
+    return [$day_of_week, $day_and_date, $weather_condition, $weather_icon, $temperature, $pressure, $wind_speed, $humidity];
+  } else {
+    echo "Error: Unable to fetch weather data.";
+  }
+}
+
+function create_DB($servername, $username, $password, $dbname)
+{
+  // Create connection
+  $conn = new mysqli($servername, $username, $password);
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  // Create database
+  $sql = "CREATE DATABASE IF NOT EXISTS $dbname";
+  if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully<br>";
+    // Select the database
+    $conn->select_db($dbname);
+  } else {
+    echo "Error creating database: " . $conn->error;
+  }
+
+  $conn->close();
+}
+
+function create_table($servername, $username, $password, $dbname)
+{
+  global $city_name; // Make the $city_name variable accessible within the function
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  // sql to create table
+  $sql = "CREATE TABLE if not exists $city_name(
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Day_of_Week VARCHAR(15),
+    Day_and_Date VARCHAR(20),
+    Weather_Condition VARCHAR(50),
+    Weather_Icon VARCHAR(100),
+    Temperature INT(5),
+    Pressure DECIMAL(6, 2),
+    Wind_Speed DECIMAL(5, 2),
+    Humidity DECIMAL(5, 2)
+  )";
+
+  if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully<br>";
+  } else {
+    echo "Error creating table: " . $conn->error;
+  }
+
+  $conn->close();
+}
+
+function update_data($servername, $username, $password, $dbname)
+{
+  global $city_name;
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  // Accessing each value of tuple
+  list($day_of_week, $day_and_date, $weather_condition, $weather_icon, $temperature, $pressure, $wind_speed, $humidity) = fetch_weather_data();
+
+  // sql to insert data to table
+  $sql = "UPDATE $city_name 
+  SET 
+      Day_and_Date = '$day_and_date',
+      Weather_Condition = '$weather_condition',
+      Weather_Icon = '$weather_icon',
+      Temperature = $temperature,
+      Pressure = $pressure,
+      Wind_Speed = $wind_speed,
+      Humidity = $humidity
+  WHERE Day_of_Week = $day_of_week";
+
+
+  if ($conn->multi_query($sql) === TRUE) {
+    echo "Records updated successfully<br>";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+
+  $conn->close();
+}
+
+function connect_DB()
+{
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "City_Weather";
+
+  // Create database
+  create_DB($servername, $username, $password, $dbname);
+
+  // Create table
+  create_table($servername, $username, $password, $dbname);
+
+  // Insert data to table
+  update_data($servername, $username, $password, $dbname);
+}
+
+connect_DB();
+?>
